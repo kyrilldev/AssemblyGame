acd37494fd95de2f42efaebe905d898c250c6cf7
Kyrill15 <52017853+kyrilldev@users.noreply.github.com>
2023-09-20 12:09:21 +0200

    sliding, choice script, bunch of switches

--- Assets/Scripts/Choice.cs
+++ Assets/Scripts/Choice.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Choice : MonoBehaviour
 {
     public int choice;
+    private Button self;
+
+    private void Start()
+    {
+        self = GetComponent<Button>();
+    }
+
+    private void Update()
+    {
+    }
 }
