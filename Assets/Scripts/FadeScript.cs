144512690aee85246bc039afabf08342f5b44721
Chieltjee <chieleikelenboom@outlook.com>
2023-09-20 11:50:27 +0200

    Fade added to main menu

---
+++ Assets/Scripts/FadeScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour
+{
+    public float fadeDuration = 1.0f; // Duration of the fade effect in seconds
+    [HideInInspector] public CanvasGroup mainCanvasGroup;
+    public CanvasGroup introCanvasGroup1;
+    public CanvasGroup introCanvasGroup2;
+
+    private void Start()
+    {
+        mainCanvasGroup = GetComponent<CanvasGroup>();
+    }
+    public IEnumerator FadeIn(CanvasGroup group)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            group.alpha = alpha;
+            yield return null;
+        }
+
+        group.interactable = false; // Disable interaction while faded out
+        group.blocksRaycasts = false; // Disable raycasts while faded out
+    }
+
+    public IEnumerator FadeOut(CanvasGroup group)
+    {
+        group.interactable = true; // Enable interaction while fading in
+        group.blocksRaycasts = true; // Enable raycasts while fading in
+
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(0f, 1f, elapsedTime / fadeDuration);
+            group.alpha = alpha;
+            yield return null;
+        }
+    }
+
+}
