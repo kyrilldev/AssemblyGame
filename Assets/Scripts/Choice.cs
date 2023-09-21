a610349492379e8463f58876ed44ac1dadf5cfc7
KyrillGit <kyrilloosterveenbva@gmail.com>
2023-09-21 09:57:11 +0200

    Sound effects

--- Assets/Scripts/Choice.cs
+++ Assets/Scripts/Choice.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Choice : MonoBehaviour
 {
     public int choice;
+    public AudioSource source;
+    public void SoundEffect()
+    {
+        source.Play();
+    }
 }
